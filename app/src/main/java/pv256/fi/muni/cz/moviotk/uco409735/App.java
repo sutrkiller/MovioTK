4861aa4abd983a9be5aa5044f44ca955be628345
sutrkiller <tobias.kamenicky@gmail.com>
2016-09-20 09:38:14 +0200

    Initial commit

---
+++ app/src/main/java/pv256/fi/muni/cz/moviotk/uco409735/App.java
@@ -0,0 +1,38 @@
+package pv256.fi.muni.cz.moviotk.uco409735;
+
+import android.app.Application;
+import android.os.Build;
+import android.os.StrictMode;
+
+/**
+ * Created by Tobias on 9/20/2016.
+ */
+
+public class App extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        if (BuildConfig.DEBUG) {
+            initStrictMode();
+        }
+    }
+
+    private void initStrictMode() {
+        StrictMode.ThreadPolicy.Builder tpb = new StrictMode.ThreadPolicy.Builder()
+                .detectAll()
+                .penaltyLog();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+            tpb.penaltyFlashScreen();
+        }
+        StrictMode.setThreadPolicy(tpb.build());
+
+        StrictMode.VmPolicy.Builder vmpb = new StrictMode.VmPolicy.Builder()
+                .detectLeakedSqlLiteObjects()
+                .penaltyLog();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+            vmpb.detectLeakedClosableObjects();
+        }
+        StrictMode.setVmPolicy(vmpb.build());
+    }
+}
---
+++ app/src/main/java/pv256/fi/muni/cz/moviotk/uco409735/MainActivity.java
@@ -0,0 +1,13 @@
+package pv256.fi.muni.cz.moviotk.uco409735;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
