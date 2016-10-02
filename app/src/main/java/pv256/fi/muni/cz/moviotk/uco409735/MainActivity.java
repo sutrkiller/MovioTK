ee44caa2f3df799d2b66660b176846e3301afee5
sutrkiller <tobias.kamenicky@gmail.com>
2016-10-02 19:09:44 +0200

    ukol2 - revised

--- app/src/main/java/pv256/fi/muni/cz/moviotk/uco409735/MainActivity.java
+++ app/src/main/java/pv256/fi/muni/cz/moviotk/uco409735/MainActivity.java
@@ -1,5 +1,6 @@
 package pv256.fi.muni.cz.moviotk.uco409735;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -13,12 +14,13 @@ public class MainActivity extends AppCompatActivity {
 
     private SharedPreferences prefs;
     private static final String PREF_THEME = "PREF_THEME";
+    private static final String APP_NAME = "MovioTK";
     private int currentTheme;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        prefs = getPreferences(MODE_PRIVATE);
+        prefs = getSharedPreferences(APP_NAME,MODE_PRIVATE);
         currentTheme = prefs.getInt(PREF_THEME, R.style.Theme);
         setTheme(currentTheme);
         setContentView(R.layout.activity_main);
@@ -26,10 +28,12 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    public void button_onClick(View view) {
+    public void buttonOnClick(View view) {
         int newTheme = currentTheme == R.style.Theme ? R.style.Theme_ThemeSecondary : R.style.Theme;
         prefs.edit().putInt(PREF_THEME, newTheme).apply();
 
-        recreate();
+        Intent intent = getIntent();
+        finish();
+        startActivity(intent);
     }
 }
