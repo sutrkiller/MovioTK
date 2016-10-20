5b9cb504ed601d730315c76da50e55fc6b1fc37a
sutrkiller <tobias.kamenicky@gmail.com>
2016-10-20 21:22:39 +0200

    ukol4 + fixed ukol3

--- app/src/main/java/pv256/fi/muni/cz/moviotk/uco409735/MainFragment.java
+++ app/src/main/java/pv256/fi/muni/cz/moviotk/uco409735/MainFragment.java
@@ -1,19 +1,26 @@
 package pv256.fi.muni.cz.moviotk.uco409735;
 
 import android.content.Context;
+import android.net.ConnectivityManager;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.ViewStubCompat;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
 
 import pv256.fi.muni.cz.moviotk.uco409735.Adapters.MovieRecyclerViewAdapter;
+import pv256.fi.muni.cz.moviotk.uco409735.Adapters.SimpleSectionedRecyclerViewAdapter;
 
 /**
  * Main fragment shows in main activity.
@@ -32,7 +39,7 @@ public class MainFragment extends Fragment {
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
-
+        Log.i(MainFragment.class.getName(),"onAttach");
         try {
             mListener = (OnMovieSelectedListener) context;
         } catch (ClassCastException e) {
@@ -43,54 +50,102 @@ public class MainFragment extends Fragment {
     @Override
     public void onDetach() {
         super.onDetach();
-
+        Log.i(MainFragment.class.getName(),"onDetach");
         mListener = null;
     }
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        Log.i(MainFragment.class.getName(),"onCreate");
         mContext = getActivity().getApplicationContext();
     }
+
+    //private boolean test = false;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_main, container,false);
+        super.onCreateView(inflater,container,savedInstanceState);
+        Log.i(MainFragment.class.getName(),"onCreateView");
 
-        fillRecyclerView(view);
+        final View view = inflater.inflate(R.layout.fragment_main, container,false);
+
+        Map<String,ArrayList<Movie>> movies = MovieData.getInstance().getMovieMap();
+        fillRecyclerView(view,movies);
 
         if(savedInstanceState != null && savedInstanceState.containsKey(SELECTED_KEY)) {
             mPosition = savedInstanceState.getInt(SELECTED_KEY);
 
             if(mPosition != RecyclerView.NO_POSITION) {
-                mRecyclerView.smoothScrollToPosition(mPosition);
+                SimpleSectionedRecyclerViewAdapter adapter = (SimpleSectionedRecyclerViewAdapter) mRecyclerView.getAdapter();
+                mRecyclerView.smoothScrollToPosition(adapter.positionToSectionedPosition(mPosition));
             }
         }
+
+//        FloatingActionButton but = (FloatingActionButton) view.findViewById(R.id.testButton);
+//        but.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                fillRecyclerView(view,test ? MovieData.getInstance().getMovieList() : null);
+//                test = !test;
+//
+//            }
+//        });
+
 
         return view;
     }
 
     @Override
     public void onSaveInstanceState(Bundle outState) {
+        Log.i(MainFragment.class.getName(),"onSaveInstanceState");
         if(mPosition != RecyclerView.NO_POSITION) {
             outState.putInt(SELECTED_KEY,mPosition);
         }
         super.onSaveInstanceState(outState);
     }
 
-    private void fillRecyclerView(View rootView) {
-        ArrayList<Movie> movies = MovieData.getInstance().getMovieList();
+    private void fillRecyclerView(View rootView, Map<String,ArrayList<Movie>> movies) {
+        //ArrayList<Movie> movies = MovieData.getInstance().getMovieList();
+        //movies = null;
 
         mRecyclerView = (RecyclerView)rootView.findViewById(R.id.recyclerView_movies);
+        ViewStubCompat viewStubEmpty = (ViewStubCompat) rootView.findViewById(R.id.recyclerView_movies_empty);
+        View inflated = rootView.findViewById(R.id.recyclerView_movies_empty_infl);
 
         if(movies != null && !movies.isEmpty()) {
+            if (inflated != null) inflated.setVisibility(View.GONE);
+            mRecyclerView.setVisibility(View.VISIBLE);
             setAdapter(mRecyclerView,movies);
+        } else {
+            if (inflated == null)inflated = viewStubEmpty.inflate();
+            TextView textView = (TextView) inflated.findViewById(R.id.recyclerView_movies_empty_text);
+            String text = isNetworkConnected() ? "No movies available." : "Internet connection not available";
+
+
+            textView.setText(text);
+            inflated.setVisibility(View.VISIBLE);
+            mRecyclerView.setVisibility(View.GONE);
         }
     }
 
-    private void setAdapter(RecyclerView movieRV, final ArrayList<Movie> movies) {
+    private boolean isNetworkConnected() {
+        ConnectivityManager cm = (ConnectivityManager) mContext.getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        return cm.getActiveNetworkInfo() != null;
+    }
+
+    private void setAdapter(RecyclerView movieRV, final Map<String,ArrayList<Movie>> moviesMap) {
+        final ArrayList<Movie> movies = new ArrayList<>();
+        ArrayList<String> keys = new ArrayList<>(moviesMap.keySet());
+        Collections.sort(keys);
+        List<SimpleSectionedRecyclerViewAdapter.Section> sections = new ArrayList<>();
+        for (String key : keys) {
+            sections.add(new SimpleSectionedRecyclerViewAdapter.Section(movies.size(),key));
+            movies.addAll(moviesMap.get(key));
+        }
+
         MovieRecyclerViewAdapter.OnItemClickListener listener = new MovieRecyclerViewAdapter.OnItemClickListener() {
             @Override
             public void onItemClick(Movie movie) {
@@ -105,7 +160,24 @@ public class MainFragment extends Fragment {
         };
 
         MovieRecyclerViewAdapter adapter = new MovieRecyclerViewAdapter(mContext,movies,listener);
-        movieRV.setAdapter(adapter);
+
+
+//        int location = 0;
+//        for (int i=0;i<moviesMap.keySet().size()) {
+//            sections.add(new SimpleSectionedRecyclerViewAdapter.Section(location,moviesMap.keySet().));
+//        }
+//        sections.add(new SimpleSectionedRecyclerViewAdapter.Section(0,"Section 1"));
+//        sections.add(new SimpleSectionedRecyclerViewAdapter.Section(2,"Section 2"));
+
+        SimpleSectionedRecyclerViewAdapter.Section[] dummy = new SimpleSectionedRecyclerViewAdapter.Section[sections.size()];
+        SimpleSectionedRecyclerViewAdapter mSectionedAdapter = new SimpleSectionedRecyclerViewAdapter(R.layout.list_section_movie,R.id.list_section_title,adapter);
+        mSectionedAdapter.setSections(sections.toArray(dummy));
+
+        if (movieRV.getAdapter() != null) {//
+            movieRV.swapAdapter(mSectionedAdapter,true);//
+        } else {//
+            movieRV.setAdapter(mSectionedAdapter);
+        }//
     }
 
 
