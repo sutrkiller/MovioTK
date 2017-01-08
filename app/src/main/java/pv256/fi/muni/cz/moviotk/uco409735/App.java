67127064d985a2b5e8c236a1c9db56f3c197fbef
sutrkiller <tobias.kamenicky@gmail.com>
2017-01-08 20:57:53 +0100

    Ukol9 finished - test fix

--- app/src/main/java/pv256/fi/muni/cz/moviotk/uco409735/App.java
+++ app/src/main/java/pv256/fi/muni/cz/moviotk/uco409735/App.java
@@ -1,7 +1,9 @@
 package pv256.fi.muni.cz.moviotk.uco409735;
 
+import android.app.Activity;
 import android.app.Application;
 import android.os.Build;
+import android.os.Bundle;
 import android.os.StrictMode;
 
 /**
@@ -9,15 +11,19 @@ import android.os.StrictMode;
  * @author Tobias Kamenicky <tobias.kamenicky@gmail.com>
  */
 
-public class App extends Application {
+public class App extends Application implements Application.ActivityLifecycleCallbacks {
+   private Activity mCurrent;
+
     @Override
     public void onCreate() {
         super.onCreate();
-
+        registerActivityLifecycleCallbacks(this);
         if (BuildConfig.DEBUG) {
             initStrictMode();
         }
     }
+
+
 
     private void initStrictMode() {
         StrictMode.ThreadPolicy.Builder tpb = new StrictMode.ThreadPolicy.Builder()
@@ -35,5 +41,45 @@ public class App extends Application {
             vmpb.detectLeakedClosableObjects();
         }
         StrictMode.setVmPolicy(vmpb.build());
+    }
+
+    @Override
+    public void onActivityCreated(Activity activity, Bundle bundle) {
+        mCurrent = activity;
+    }
+
+    @Override
+    public void onActivityStarted(Activity activity) {
+        mCurrent = activity;
+
+    }
+
+    @Override
+    public void onActivityResumed(Activity activity) {
+        mCurrent = activity;
+    }
+
+    @Override
+    public void onActivityPaused(Activity activity) {
+
+    }
+
+    @Override
+    public void onActivityStopped(Activity activity) {
+
+    }
+
+    @Override
+    public void onActivitySaveInstanceState(Activity activity, Bundle bundle) {
+
+    }
+
+    @Override
+    public void onActivityDestroyed(Activity activity) {
+
+    }
+
+    public Activity getCurrentActivity() {
+        return mCurrent;
     }
 }
